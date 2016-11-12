7b823e7b21a74e1e6ec3f77fee08d4cf339c142b
Venkat Sampath <vsampath@visionit.com>
2016-11-11 22:59:50 -0500

    changes

--- src/myController.js
+++ src/myController.js
@@ -18,10 +18,12 @@ function ControllerOne($scope, $rootScope, $http, $window) {
             return (requestData.then(function(response) {
 				if(response.data['NA Response'] === "https://tableau-cloud.login.covapp.io/login.do?host=https://tableau-cloud.portal.covapp.io"){
 					//$scope.message ="Please check your email.";
-					$window.open ("https://tableau-cloud.portal.covapp.io", "_blank");
+					//$window.open ("https://tableau-cloud.portal.covapp.io", "_blank");
+					$window.location.href = "https://tableau-cloud.login.covapp.io/login.do?host=https://tableau-cloud.portal.covapp.io";
 				}
 				else if(response.data['ACME Response'] === "https://tableau2-acme.login.eu1.covapp.io/login.do?host=https://tableau-cloud.portal.covapp.io"){
-					$window.open("https://tableau2-acme.portal.eu1.covapp.io/", "_blank");
+					//$window.open("https://tableau2-acme.portal.eu1.covapp.io/", "_blank");
+					$window.location.href = "https://tableau2-acme.login.eu1.covapp.io/login.do?host=https://tableau2-acme.portal.eu1.covapp.io";
 				} 
                 else {
 					$scope.message ="Email not registered with us.";
